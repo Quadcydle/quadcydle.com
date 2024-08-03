66e804d8935bb8e5dd7c095de08fc5ce597e7852
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-08-03 15:59:33 +0530

    fix: Header alignment

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 z-10 w-full border-b border-transparent-white backdrop-blur-[12px]">
-      <Container className="flex h-navigation-height">
+      <Container className="flex justify-between h-navigation-height">
         <Link className="flex items-center text-md" href="/">
           <Logo className="mr-4 h-[1.8rem] w-[1.8rem]" /> Linear
         </Link>
