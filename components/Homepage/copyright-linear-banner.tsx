b943faadd70a74ccf0661bbc0ecd946ac5df16c8
dhruvagrawat <dhruvagrawat9@gmail.com>
2024-08-03 15:50:14 +0530

    Cookie Banner update

--- components/Homepage/copyright-linear-banner.tsx
+++ components/Homepage/copyright-linear-banner.tsx
@@ -27,19 +27,16 @@ export const CopyrightLinearBanner = () => {
     <div className="fixed right-[4rem] bottom-[4rem] flex w-[65rem] max-w-[95%] items-start rounded-md border border-transparent-white p-8 text-[2rem] backdrop-blur-[12px]">
       <div className="space-y-3">
         <p>
-          <strong>⚠️ NOTE:</strong> I am not affiliated with Linear in any way.
-          All images, the name of Linear and their logo are copyrighted by
-          Linear. This website is only intended for educational purposes and as
-          a homage to Linear's great UI work 🙏
+          <strong> NOTE:</strong> Cookies section Pending
         </p>
         <p>
-          <Button href="https://linear.app" size="large" target="_blank">
-            Visit Linear's website
+          <Button href="https://quadcydle.com" size="large" target="_blank">
+            Visit Quadcydle website
           </Button>
         </p>
       </div>
       <button className="ml-8" onClick={dismissCookie}>
-        ✕ <span className="sr-only">Dimiss copyright banner</span>
+        ✕ <span className="sr-only">Hide</span>
       </button>
     </div>
   );
